d60876ced5c5f921e7ff7391dd31e22bce9f6dfe
VascoPanigi <156548239+VascoPanigi@users.noreply.github.com>
2024-07-10 15:36:31 +0200

    repo setup

---
+++ src/main/java/vascopanigi/MoneyZen/MoneyZenApplication.java
@@ -0,0 +1,13 @@
+package vascopanigi.MoneyZen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyZenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyZenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vascopanigi/MoneyZen/MoneyZenApplicationTests.java
@@ -0,0 +1,13 @@
+package vascopanigi.MoneyZen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyZenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
