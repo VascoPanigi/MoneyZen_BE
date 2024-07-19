165e691fd849926eaea04ad9ab371cf474ef5089
VascoPanigi <156548239+VascoPanigi@users.noreply.github.com>
2024-07-19 10:42:27 +0200

    add userPlan Entity

--- src/main/java/vascopanigi/MoneyZen/entities/UserPlan.java
+++ src/main/java/vascopanigi/MoneyZen/entities/UserPlan.java
@@ -1,4 +1,19 @@
 package vascopanigi.MoneyZen.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "user_plan")
 public class UserPlan {
+    @Id
+    @GeneratedValue
+    @Setter(AccessLevel.NONE)
+    private UUID id;
 }
